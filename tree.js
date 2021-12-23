aaadf39314b95bd5d0d4d75fddea01d810f9d212
reid <reidbras@gmail.com>
2021-12-22 21:01:36 -0600

    Adding christmas tree script.

---
+++ tree.js
@@ -0,0 +1,10 @@
+class ChristmasTree {
+    constructor(height, width);
+
+    generate(heignt) {
+        document.write("");
+        for(let i=1; i <= height; i++) {
+            
+        }
+    }
+}
