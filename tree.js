7283b35af81031f81e5a33d7ef64cfa31cbb3a99
reid <reidbras@gmail.com>
2021-12-23 21:38:43 -0600

    Adding code for tree

--- tree.js
+++ tree.js
@@ -1,13 +1,55 @@
 class ChristmasTree {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
+    constructor() {
     }
 
-    generate() {
-        document.write("");
+    generate(height) {
+
+        let widthCounter = 1;
+        let maxWidth = (height * 2);
+        let center = (maxWidth / 2) + 1;
+
+        document.write("<pre>");
         for(let i=1; i <= height; i++) {
-            
+
+            let indent = height - i;
+
+            // Print indent
+            for(let j=1; j<= indent; j++) {
+                document.write("&nbsp")
+            }
+            // Print stars
+            for(let j=1; j <= widthCounter; j++) {
+                document.write("<span>*</span>");
+            }
+
+            // New Row of stars
+            document.write("<br>");
+
+            // Increase width for next row
+            widthCounter = widthCounter + 2;
         }
+        // Lower End of Tree
+        for(let i=0; i < (widthCounter -2); i++) {
+            document.write("<span>-</span>");
+        }
+
+        document.write("<br>");
+
+        // Trunk of Tree
+        for(let i=0; i < 2; i++) {
+            for(let j=0; j <= center; j++) {
+                if(j == center) {
+                    document.write("<span>\"</span>");
+                    document.write("<br>");
+                }
+                else {
+                    document.write("&nbsp;");
+                }
+
+            }
+        }
+        document.write("</pre>");
     }
 }
+let tree = new ChristmasTree();
+tree.generate(20);
